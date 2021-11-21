9fdb7f867ef99bcbddfb2a70366ec1a2fd2d3ec7
Christos Christidis <49539491+FSBgr@users.noreply.github.com>
2021-11-21 16:48:48 +0200

    first day, neural network built, TODOS inside

--- Neural.py
+++ Neural.py
@@ -26,12 +26,25 @@ plt.show()
 
 class Net(nn.Module):
     def __init__(self):
-        super().__init__()  # Adding 4 layers, TODO: Change the number of layers, number of outputs etc for testing and result reports
+        super().__init__()  # Adding 4 fully connected layers, TODO: Change the number of layers, number of outputs etc for testing and result reports
         self.fc1 = nn.Linear(28 * 28, 64)  # 28 by 28 picture input
         self.fc2 = nn.Linear(64, 64)  # input is 64 since previous layer output is 64
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 10)  # Last layer output is 10, for the 10 single digit numbers
 
+    def forward(self, x):
+        x = F.relu(self.fc1(x))   # F.relu being the activation function, TODO: Change the functions through each layer and then compare in the end
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1) # returning softmax output of last layer
 
+# TODO: initialize weights randomly
 net = Net()
-print(net)
+#print(net)
+
+X = torch.rand((28, 28))
+X = X.view(-1, 28*28) # flattening the tensor to fit the libraries, -1 specifies that input will be of unknown shape
+#print(X)
+output = net(X)
+print(output)
