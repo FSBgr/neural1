fa39c562aff5962c5b4a445868e76599656ba536
Christos Christidis <49539491+FSBgr@users.noreply.github.com>
2021-11-26 19:55:26 +0200

    Add files via upload

--- Neural.py
+++ Neural.py
@@ -13,17 +13,6 @@ test = datasets.MNIST("", train=True, download=True, transform=transforms.Compos
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)  # Shuffling for better distribution
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 
-'''for data in trainset:
-    print(data)
-    break
-
-x, y = data[0][0], data[1][0]
-
-plt.imshow(data[0][0].view(28, 28))
-plt.show()
-# print(data[0][0].shape)
-'''
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -34,23 +23,18 @@ class Net(nn.Module):
         self.fc4 = nn.Linear(64, 10)  # Last layer output is 10, for the 10 single digit numbers
 
     def forward(self, x):
-        x = F.relu(self.fc1(
-            x))  # F.relu being the activation function, TODO: Change the functions through each layer and then compare in the end
+        x = F.relu(self.fc1(x))  # F.relu being the activation function, TODO: Change the functions through each layer and then compare in the end
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return F.log_softmax(x, dim=1)  # returning softmax output of last layer
 
 
-# TODO: initialize weights randomly
 net = Net()
-# print(net)
 
 X = torch.rand((28, 28))
 X = X.view(-1, 28 * 28)  # flattening the tensor to fit the libraries, -1 specifies that input will be of unknown shape
-# print(X)
 output = net(X)
-# print(output)
 
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 EPOCHS = 3
@@ -63,7 +47,6 @@ for epoch in range(EPOCHS):
         loss = F.nll_loss(output, y)
         loss.backward()
         optimizer.step()
-    # print(loss)
 
 correct = 0
 total = 0
@@ -76,4 +59,9 @@ with torch.no_grad():
             if torch.argmax(i) == y[idx]:  # comparing the accuracy, for every prediction we make, does it match the actual value
                 correct += 1
             total += 1
-print("Accuracy: ", round(correct / total, 3))
+
+
+print("Accuracy: ", round(correct / total, 3)) # printing results
+plt.imshow(X[0].view(28, 28))
+plt.show()
+print(torch.argmax(net(X[0].view(-1, 28 * 28))[0]))
