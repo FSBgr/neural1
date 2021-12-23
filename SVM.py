43e276fede7106e83358bd19a60de8d7027a618f
Christos Christidis <49539491+FSBgr@users.noreply.github.com>
2021-12-23 16:36:26 +0200

    Add files via upload

--- SVM.py
+++ SVM.py
@@ -7,9 +7,8 @@ digits = datasets.load_digits()  # loading the 0-9 digits dataset
 trainingSize = round(len(digits.data) * 0.6)  # declaring the size of the training set and the test set
 testingSize = round(len(digits.data) * 0.4)
 
-
 classifier = svm.SVC(gamma=0.001, C=100)  # also try for gamma=0.1 and gamma=0.0001
-# classifier = svm.SVC(kernel="rbf")  # also try kernel="poly", kernel="rbf", kernel="linear"
+# classifier = svm.SVC(kernel="sigmoid")  # also try kernel="poly", kernel="rbf", kernel="linear"
 
 startTime = time.time()
 trainingSamples, trainingLabels = digits.data[:trainingSize], digits.target[:trainingSize]
@@ -19,12 +18,13 @@ print("Training was completed in: ", finishTime - startTime, " seconds.")
 
 # toggle on the following for loop to print the images and compare them with the SVM's guesses
 
-# for i in range(trainingSize):
-#   print("Prediction: ", classifier.predict(digits.data[i].reshape(1, -1)), " Actual Value: ", digits.target[i])
-#   plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation="nearest")
-
 testSamples = digits.data[trainingSize + 1: len(digits.data)]  # using the rest 40% of the set as testing set
 testLabels = digits.target[trainingSize + 1: len(digits.data)]
+
+for i in range(testingSize-1):
+    print("Prediction: ", classifier.predict(testSamples[i].reshape(1, -1)), " Actual Value: ", testLabels[i])
+#   plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation="nearest")
+
 print("The training accuracy is: ", classifier.score(trainingSamples, trainingLabels))
 startTime = time.time()
 print("The prediction/testing accuracy is: ", classifier.score(testSamples, testLabels))
